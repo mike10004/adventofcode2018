5e56872d4c2a76a2e78d846a4a2c1d8a0c624c7b
Mike Chaberski <mchaberski@novetta.com>
2018-12-18 17:52:16 -0500

    clean up tests

--- day08/test_navtree.mjs
+++ day08/test_navtree.mjs
@@ -2,11 +2,25 @@ import {describe, it} from '../common/exams/lib/exams';
 import assert from 'assert';
 import { Node, Parser } from './navtree';
 
+function getSampleNodeList() {
+    const parser = Parser.fromText("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2");
+    return parser.consumeAll();
+}
+
 describe("Parser", () => {
+
     it("consumeAll", () => {
         const parser = Parser.fromText("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2");
         const nodes = parser.consumeAll();
         assert.equal(nodes.length, 4);
+    });
+    
+});
+
+describe("Node", () => {
+
+    it("sumMetadatas", () => {
+        const nodes = getSampleNodeList();
         let metadataSum = 0;
         nodes.forEach(node => {
             // process.stderr.write("\n\n");
@@ -20,14 +34,12 @@ describe("Parser", () => {
         assert.notEqual(typeof(root), 'undefined');
         assert.equal(root.sumMetadatas(true), 138);
     });
-});
 
-describe("Node", () => {
     it("value", () => {
-        const parser = Parser.fromText("2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2");
-        const nodes = parser.consumeAll();
+        const nodes = getSampleNodeList();
         const root = Node.findRoot(nodes);
         assert.notEqual(typeof(root), 'undefined');
         assert.equal(root.value(), 66);
     });
+
 });
