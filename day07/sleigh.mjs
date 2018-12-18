e6b2588db4dc85c5168cb1c4e1577dffab6e282a
Mike Chaberski <mchaberski@novetta.com>
2018-12-18 12:45:27 -0500

    day 7 part 1: implement sequencing function

--- day07/sleigh.mjs
+++ day07/sleigh.mjs
@@ -18,21 +18,81 @@ export class Step {
         this.dependencies.add(stepId);
     }
 
+    /**
+     * 
+     * @param {Set} completed set of completed step IDs
+     */
     isDoable(completed) {
         for (let dep of this.dependencies) {
-            if (!ArrayUtils.contains(completed, dep)) {
+            if (!completed.has(dep)) {
                 return false;
             }
         }
         return true;
     }
 
+    static parseAll(text) {
+        const steps = {};
+        for (let line of text.split("\n")) {
+            if (line) {
+                const m = /^Step (\w+) must be finished before step (\w+) can begin/.exec(line);
+                if (m) {
+                    const id = m[2];
+                    const dep = m[1];
+                    let step = steps[id];
+                    if (!step) {
+                        step = new Step(id);
+                        steps[id] = step;
+                    }
+                    step.addDependency(dep);
+                    if (!(dep in steps)) {
+                        steps[dep] = new Step(dep);
+                    }
+                } else {
+                    console.info("does not match expected pattern", line);
+                }
+            }
+        }
+        return steps;
+    }
+
 }
 
 export class Sequencer {
 
+    /**
+     * 
+     * @param {object} steps an object whose keys are step IDs and whose values are Step objects
+     */
     compute(steps) {
-        throw new Error("not yet implemented");
+        const numSteps = Object.keys(steps).length;
+        const completed = new Set();
+        const sequence = [];
+        while (sequence.length < numSteps) {
+            const doable = this.findDoable(steps, completed);
+            if (doable.length === 0) {
+                console.info("not doable");
+                return null;
+            }
+            const id = doable[0];
+            sequence.push(id);
+            completed.add(id);
+        }
+        return sequence;
+    }
+
+    findDoable(steps, completed) {
+        const doable = [];
+        for (let id in steps) {
+            if (!completed.has(id)) {
+                const step = steps[id];
+                if (step.isDoable(completed)) {
+                    doable.push(id);
+                }
+            }
+        }
+        doable.sort();
+        return doable;
     }
 
 }
--- day07/test_sleigh.mjs
+++ day07/test_sleigh.mjs
@@ -2,8 +2,37 @@ import {describe, it} from '../common/exams/lib/exams';
 import assert from 'assert';
 import { ArrayUtils, Step, Sequencer } from './sleigh';
 
-describe("ArrayUtils", function() {
-    it("contains", function() {
+describe("ArrayUtils", () => {
+    it("contains", () => {
         assert.equal(ArrayUtils.contains([1, 2, 3], 2), true);
+    });
+});
+
+const SAMPLE_TEXT = "Step C must be finished before step A can begin.\n" + 
+"Step C must be finished before step F can begin.\n" + 
+"Step A must be finished before step B can begin.\n" + 
+"Step A must be finished before step D can begin.\n" + 
+"Step B must be finished before step E can begin.\n" + 
+"Step D must be finished before step E can begin.\n" + 
+"Step F must be finished before step E can begin.\n";
+
+describe("Step", () => {
+
+    it("parseAll", () => {
+        const text = SAMPLE_TEXT;
+        const steps = Step.parseAll(text);
+        assert.equal("ABCDEF".length, Object.keys(steps).length);
+        Object.values(steps).forEach(step => {
+            assert.notEqual(typeof(step.id), "undefined"); 
+        })
+    });
+
+});
+
+describe("Sequencer", () => {
+    it("compute", () => {
+        const steps = Step.parseAll(SAMPLE_TEXT);
+        const sequence = new Sequencer().compute(steps);
+        assert.deepEqual(sequence, ['C', 'A', 'B', 'D', 'F', 'E']);
     });
 });
