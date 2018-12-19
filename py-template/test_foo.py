5de92f036a3100193e74265b3a889a312c7ff5c6
Mike Chaberski <mchaberski@novetta.com>
2018-12-19 14:12:18 -0500

    support logging in unit tests in python project template

--- py-template/test_foo.py
+++ py-template/test_foo.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
-import unittest
+import os
 import io
+import sys
+import logging
+import unittest
+
+_log = logging.getLogger(__name__)
+_logging_configured = False
+
+if not _logging_configured:
+    level_str = os.getenv('UNIT_TEST_LOG_LEVEL') or 'INFO'
+    level = logging.__dict__.get(level_str, 'INFO')
+    logging.basicConfig(level=level)
+    _log.debug("logging configured at level %s (%s)", level, level_str)
+    logging_configured = True
 
 
 class TestProcessor(unittest.TestCase):
