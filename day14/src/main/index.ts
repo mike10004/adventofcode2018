08b689c2c629e92a16b11fd31d102dce7fd4573e
Mike Chaberski <mchaberski@novetta.com>
2019-01-07 11:41:29 -0500

    day 14 part one complete (fixed interpretation of question)

--- day14/src/main/index.ts
+++ day14/src/main/index.ts
@@ -2,17 +2,18 @@ import {Elf, Scoreboard, RecipeMachine} from './chocolate';
 import 'process';
 
 console.debug("argv", process.argv);
-const input = process.argv[2] || '637061';
-const iterations = parseInt(process.argv[3] || '10');
-const scoreboard = Scoreboard.create(input);
-const elves = Elf.createArray(2);
+const goodThreshold = parseInt(process.argv[2] || '637061', 10);
+const numSubsequent = parseInt(process.argv[3] || '10', 10);
+const iterations = goodThreshold + numSubsequent;
+const scoreboard = new Scoreboard([3, 7]);
+const elves = Elf.createArray(scoreboard.length());
 const machine = new RecipeMachine(scoreboard);
 
 for (let i = 0; i < iterations; i++) {
     machine.combine(elves);
-    process.stdout.write(scoreboard.render(elves));
-    process.stdout.write("\n");
+    // process.stdout.write(scoreboard.render(elves));
+    // process.stdout.write("\n");
 }
 
-const answer = scoreboard.substring(input.length, iterations);
+const answer = scoreboard.substring(goodThreshold, numSubsequent);
 process.stdout.write(answer + "\n");
